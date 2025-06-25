dacf8d8149178fb71cc1aa4fa9a17611255c13bb
haroldmud <haroldmudosa@gmail.com>
2025-06-25 17:45:59 +0200

    feat: implement tailwind

--- apps/wishlist/src/app/app-module.ts
+++ apps/wishlist/src/app/app-module.ts
@@ -3,11 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { App } from './app';
 import { appRoutes } from './app.routes';
-import { NxWelcome } from './nx-welcome';
-
 @NgModule({
-  declarations: [App, NxWelcome],
-  imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
+  imports: [App, BrowserModule, RouterModule.forRoot(appRoutes)],
   providers: [provideBrowserGlobalErrorListeners()],
   bootstrap: [App],
 })
