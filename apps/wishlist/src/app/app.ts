81aa46a2dd3431ba96686c3cb8225eb9d7d7e351
haroldmud <haroldmudosa@gmail.com>
2025-06-26 09:41:12 +0200

    feat: create the first data management

--- apps/wishlist/src/app/app.ts
+++ apps/wishlist/src/app/app.ts
@@ -1,17 +1,25 @@
 import { Component } from '@angular/core';
 import { WishItems } from '../shared/model/wishlist'
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
+  imports:[CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
-  List = [
+  List: WishItems[] = [
     new WishItems('Brand new mercedes c200'),
     new WishItems('Learn Angular', true),
     new WishItems('Finish the project')
   ]
+
   protected title = 'wishlist'
+
+  toggleItem(item: WishItems) {
+    item.isFulfilled = !item.isFulfilled
+    console.log(`${item.wishText}: ${item.isFulfilled}`)
+  }
 }
